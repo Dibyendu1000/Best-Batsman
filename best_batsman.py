524c4d1a09a6d51acfc3a3342133d9f069b50d57
Dibyendu Das <43660480+Dibyendu1000@users.noreply.github.com>
2019-01-16 08:36:36 +0530

    feat: Add menu to choose best batsman

--- best_batsman.py
+++ best_batsman.py
@@ -10,8 +10,27 @@ mask=delivery['batsman'].isin(y)
 new_delivery=delivery[mask]
 mask=new_delivery['over']>15
 mew_delivery=new_delivery[mask]
-a=mew_delivery.groupby('batsman')['batsman_runs'].sum()
-b=mew_delivery.groupby('batsman')['batsman_runs'].count()
-c=a/b
-c*=100
-print(c.sort_values(ascending=False))
+while(1):
+    print('''Menu:
+          1)Best Batsman (Best Strike Rate)
+          2)Best Batsman (Best Avg Runs)
+          3)Best Batsman (Longest Survival)
+          4)Exit''')
+    ch=int(input("Enter Choice:"))
+    if(ch==1):
+        a=mew_delivery.groupby('batsman')['batsman_runs'].sum()
+        b=mew_delivery.groupby('batsman')['batsman_runs'].count()/6
+        c=a/b
+        print(c.sort_values(ascending=False).head(5))
+    elif(ch==2):
+        a=mew_delivery.groupby('batsman')['batsman_runs'].sum()
+        b=mew_delivery.groupby('batsman')['match_id'].nunique()
+        c=a/b
+        print(c.sort_values(ascending=False).head(5))
+    elif(ch==3):
+        a=mew_delivery.groupby('batsman')['batsman_runs'].count()/6
+        b=mew_delivery.groupby('batsman')['match_id'].nunique()
+        c=a/b
+        print(c.sort_values(ascending=False).head(5))
+    elif(ch==4):
+        exit()
